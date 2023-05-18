f68186ac67cd4f04f661ecbb2a6dd6c495167b9d
Tori(토리) <68044498+khs3737@users.noreply.github.com>
2023-05-19 00:38:51 +0900

    Time: 0 ms (100.00%) | Memory: 9 MB (36.69%) - LeetSync

--- remove-element/remove-element.cpp
+++ remove-element/remove-element.cpp
@@ -2,17 +2,17 @@ class Solution {
 public:
     int removeElement(vector<int> &nums, int val) {
         queue<int> q;
-        int answer = nums.size();
-        for (int i = 0; i < nums.size(); i++) {
+        int n = nums.size();
+        int answer = n;
+        vector<int> v;
+        for (int i = 0; i < n; i++) {
             if (nums[i] == val) {
                 answer--;
-                q.push(i);
-            } else if (!q.empty()) {
-                q.push(i);
-                nums[q.front()] = nums[i];
-                q.pop();
+            } else {
+                v.push_back(nums[i]);
             }
         }
+        nums = v;
         return answer;
     }
 };
