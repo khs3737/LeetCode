ff9b45c5c0441d6031b61a41d74dd6b393be9d26
Tori(토리) <68044498+khs3737@users.noreply.github.com>
2023-05-20 02:36:37 +0900

    Time: 14 ms (82.13%) | Memory: 16.5 MB (96.91%) - LeetSync

--- jump-game-ii/jump-game-ii.cpp
+++ jump-game-ii/jump-game-ii.cpp
@@ -2,17 +2,16 @@ class Solution {
 public:
     int jump(vector<int> &nums) {
         int n = nums.size();
-        vector<int> jump_count(n);
+        int jump_count = 0;
+        int dest = 0;
+        int next_dest = 0;
         for (int i = 0; i < n; i++) {
-            jump_count[i] = 1000000;
-        }
-        jump_count[0] = 0;
-
-        for (int i = 0; i < n - 1; i++) {
-            for (int j = i + 1; j <= min(i + nums[i], n - 1); j++) {
-                jump_count[j] = min(jump_count[j], jump_count[i] + 1);
+            if (i > dest) {
+                jump_count++;
+                dest = next_dest;
             }
+            next_dest = max(next_dest, i + nums[i]);
         }
-        return jump_count[n - 1];
+        return jump_count;
     }
 };
