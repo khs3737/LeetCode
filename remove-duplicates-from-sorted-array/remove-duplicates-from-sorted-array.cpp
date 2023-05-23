f08d178e9e9b7d1d30452ce217a0091c5bc523c4
Tori(토리) <68044498+khs3737@users.noreply.github.com>
2023-05-23 22:55:35 +0900

    Time: 16 ms (39.34%) | Memory: 18.4 MB (72.24%) - LeetSync

--- remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
+++ remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
@@ -1,16 +1,13 @@
 class Solution {
 public:
     int removeDuplicates(vector<int> &nums) {
-        vector<int> v;
-        v.push_back(nums[0]);
         int answer = 1;
         for (int i = 1; i < nums.size(); i++) {
             if (nums[i - 1] != nums[i]) {
-                v.push_back(nums[i]);
+                nums[answer] = nums[i];
                 answer++;
             }
         }
-        nums = v;
         return answer;
     }
 };
